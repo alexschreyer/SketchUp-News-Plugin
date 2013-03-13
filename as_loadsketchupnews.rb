2a65fbdc8ea8e903326685f702edd97fec497683
Alexander C. Schreyer <mail@alexschreyer.net>
2013-03-13 15:23:48 -0400

    First release

---
+++ as_loadsketchupnews.rb
@@ -0,0 +1,11 @@
+# Loader for as_plugins/as_sketchupnews/as_sketchupnews.rb
+
+require 'sketchup.rb'
+require 'extensions.rb'
+
+as_sketchupnews = SketchupExtension.new "SketchUp News", "as_plugins/as_sketchupnews/as_sketchupnews.rb"
+as_sketchupnews.copyright= 'Copyright 2013 Alexander C. Schreyer'
+as_sketchupnews.creator= 'Alexander C. Schreyer, www.alexschreyer.net'
+as_sketchupnews.version = '1.0'
+as_sketchupnews.description = "This plugin adds a menu item under Help that shows SketchUp news."
+Sketchup.register_extension as_sketchupnews, true
---
+++ as_plugins/as_sketchupnews/as_sketchupnews.rb
@@ -0,0 +1,66 @@
+=begin
+
+Copyright 2013, Alexander C. Schreyer
+All rights reserved
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE.
+
+License:        GPL (http://www.gnu.org/licenses/gpl.html)
+
+Author :        Alexander Schreyer, www.alexschreyer.net, mail@alexschreyer.net
+
+Website:        http://www.alexschreyer.net/projects/sketchup-news-plugin/
+
+Name :          SketchUpNews
+
+Version:        1.0
+
+Date :          3/13/2013
+
+Description :   This plugin adds a menu item under Help that shows SketchUp news.
+
+History:        1.0 (3/13/2013):
+                - first version
+                
+
+=end
+
+
+require 'sketchup.rb'
+
+
+module AS_sketchup_news
+
+
+  def self.show_news
+  # Browse using webdialog
+  
+    dlg = UI::WebDialog.new("SketchUp News", true,
+      "SketchUp News", 750, 750, 150, 150, true);
+    dlg.set_url("http://sketchupfordesign.com/news-display/")
+    dlg.navigation_buttons_enabled = false
+    dlg.show
+    
+  end # show_news
+
+
+end # module
+
+
+
+# ====================================================
+
+
+
+if !file_loaded?(__FILE__)
+
+  # Get the SketchUp plugins menu
+  help_menu = UI.menu("Help")
+  help_menu.add_item("SketchUp News") { AS_sketchup_news::show_news }
+  
+  # Let Ruby know we have loaded this file
+  file_loaded(__FILE__)
+
+end # if
