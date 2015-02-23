f7718fe9e80cb28cab3ff54b0644dcc518f2b174
Alexander C. Schreyer <mail@alexschreyer.net>
2015-02-22 22:19:39 -0500

    Code cleanup

--- as_sketchupnews.rb
+++ as_sketchupnews.rb
@@ -1,7 +1,45 @@
-# Loader for as_sketchupnews/as_sketchupnews.rb
+=begin
+
+Copyright 2013-2015, Alexander C. Schreyer
+All rights reserved
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE.
+
+License:        GPL (http://www.gnu.org/licenses/gpl.html)
+
+Author :        Alexander Schreyer, www.alexschreyer.net, mail@alexschreyer.net
+
+Website:        http://www.alexschreyer.net/projects/sketchup-news-plugin/
+
+Name :          SketchUpNews
+
+Version:        1.1
+
+Date :          5/10/2013
+
+Description :   This plugin adds a menu item under Help that shows SketchUp news.
+
+History:        1.0 (3/13/2013):
+                - first version
+                1.1 (5/10/2013):
+                - reorganized folders
+                TBD:
+                - Code cleanup
+
+=end
+
+
+# ==================
+
 
 require 'sketchup'
 require 'extensions'
+
+
+# ==================
+
 
 as_sketchupnews = SketchupExtension.new "SketchUp News", "as_sketchupnews/as_sketchupnews.rb"
 as_sketchupnews.copyright= 'Copyright 2013 Alexander C. Schreyer'
@@ -9,3 +47,6 @@ as_sketchupnews.creator= 'Alexander C. Schreyer, www.alexschreyer.net'
 as_sketchupnews.version = '1.1'
 as_sketchupnews.description = "This plugin adds a menu item under Help that shows SketchUp news."
 Sketchup.register_extension as_sketchupnews, true
+
+
+# ==================
--- as_sketchupnews/as_sketchupnews.rb
+++ as_sketchupnews/as_sketchupnews.rb
@@ -1,68 +1,55 @@
-=begin
-
-Copyright 2013, Alexander C. Schreyer
-All rights reserved
-
-THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE.
-
-License:        GPL (http://www.gnu.org/licenses/gpl.html)
-
-Author :        Alexander Schreyer, www.alexschreyer.net, mail@alexschreyer.net
-
-Website:        http://www.alexschreyer.net/projects/sketchup-news-plugin/
-
-Name :          SketchUpNews
-
-Version:        1.1
-
-Date :          5/10/2013
-
-Description :   This plugin adds a menu item under Help that shows SketchUp news.
-
-History:        1.0 (3/13/2013):
-                - first version
-                1.1 (5/10/2013):
-                - reorganized folders
-                
-
-=end
+# ==================
+# Main file for SketchUpNews
+# ==================
 
 
 require 'sketchup.rb'
 
 
-module AS_sketchup_news
+# ==================
 
 
-  def self.show_news
-  # Browse using webdialog
-  
-    dlg = UI::WebDialog.new("SketchUp News", true,
-      "SketchUp News", 750, 750, 150, 150, true);
-    dlg.set_url("http://sketchupfordesign.com/news-display/")
-    dlg.navigation_buttons_enabled = false
-    dlg.show
-    
-  end # show_news
+module AS_Extensions
+
+  module AS_SketchupNews
 
 
-end # module
+    # ==================
 
 
+    def self.show_news
+    # Browse using webdialog
 
-# ====================================================
+      dlg = UI::WebDialog.new("SketchUp News", true,
+        "SketchUp News", 750, 750, 150, 150, true);
+      dlg.set_url("http://sketchupfordesign.com/news-display/")
+      dlg.navigation_buttons_enabled = false
+      dlg.show
+
+    end # show_news
 
 
+    # ==================
 
-if !file_loaded?(__FILE__)
 
-  # Get the SketchUp plugins menu
-  help_menu = UI.menu("Help")
-  help_menu.add_item("SketchUp News") { AS_sketchup_news::show_news }
-  
-  # Let Ruby know we have loaded this file
-  file_loaded(__FILE__)
+    if !file_loaded?(__FILE__)
 
-end # if
+      # Get the SketchUp help menu
+      help_menu = UI.menu("Help")
+      help_menu.add_item("SketchUp News") { self.show_news }
+
+      # Let Ruby know we have loaded this file
+      file_loaded(__FILE__)
+
+    end # if
+
+
+    # ==================
+
+
+  end # module AS_SketchupNews
+
+end # module AS_Extensions
+
+
+# ==================
