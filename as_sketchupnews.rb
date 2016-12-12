b30fda5ca260529bd844f58ac1ba110e0f5896ea
Alexander C. Schreyer <mail@alexschreyer.net>
2016-12-12 20:29:35 +0000

    Version 1.2 changes

--- as_sketchupnews.rb
+++ as_sketchupnews.rb
@@ -1,6 +1,6 @@
 =begin
 
-Copyright 2013-2015, Alexander C. Schreyer
+Copyright 2013-2016, Alexander C. Schreyer
 All rights reserved
 
 THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
@@ -15,38 +15,59 @@ Website:        http://www.alexschreyer.net/projects/sketchup-news-plugin/
 
 Name :          SketchUpNews
 
-Version:        1.1
+Version:        1.2
 
-Date :          5/10/2013
+Date :          12/12/2015
 
-Description :   This plugin adds a menu item under Help that shows SketchUp news.
+Description :   Adds a menu item under Help that shows SketchUp news from blogs, 
+                discussions, and Twitter.
 
 History:        1.0 (3/13/2013):
                 - first version
                 1.1 (5/10/2013):
                 - reorganized folders
-                TBD:
+                1.2 (12/12/2015):
                 - Code cleanup
+                - Fixed loader code
+                - Implemented new dialog for 2017
 
 =end
 
 
-# ==================
+# ========================
 
 
-require 'sketchup'
-require 'extensions'
+require 'sketchup.rb'
+require 'extensions.rb'
 
 
-# ==================
+# ========================
 
 
-as_sketchupnews = SketchupExtension.new "SketchUp News", "as_sketchupnews/as_sketchupnews.rb"
-as_sketchupnews.copyright= 'Copyright 2013 Alexander C. Schreyer'
-as_sketchupnews.creator= 'Alexander C. Schreyer, www.alexschreyer.net'
-as_sketchupnews.version = '1.1'
-as_sketchupnews.description = "This plugin adds a menu item under Help that shows SketchUp news."
-Sketchup.register_extension as_sketchupnews, true
+module AS_Extensions
+
+  module AS_SketchupNews
+  
+    @extversion           = "1.2"
+    @exttitle             = "SketchUp News"
+    @extname              = "as_sketchupnews"
+    
+    @extdir = File.dirname(__FILE__)
+    @extdir.force_encoding('UTF-8') if @extdir.respond_to?(:force_encoding)
+    
+    loader = File.join( @extdir , @extname , "as_sketchupnews.rb" )
+   
+    extension             = SketchupExtension.new( @exttitle , loader )
+    extension.copyright   = "Copyright 2013-#{Time.now.year} Alexander C. Schreyer"
+    extension.creator     = "Alexander C. Schreyer, www.alexschreyer.net"
+    extension.version     = @extversion
+    extension.description = "Adds a menu item under Help that shows SketchUp news from blogs, discussions, and Twitter."
+    
+    Sketchup.register_extension( extension , true )
+         
+  end  # module AS_SketchupNews
+  
+end  # module AS_Extensions
 
 
-# ==================
+# ========================
--- as_sketchupnews/as_sketchupnews.rb
+++ as_sketchupnews/as_sketchupnews.rb
@@ -15,16 +15,31 @@ module AS_Extensions
 
 
     # ==================
+    
+    def self.show_url( title , url )
+    # Show website either as a WebDialog or HtmlDialog
+    
+      if Sketchup.version.to_f < 17 then  # Use old method
+        d = UI::WebDialog.new( title , true ,
+          title.gsub(/\s+/, "_") , 1000 , 600 , 100 , 100 , true);
+        d.navigation_buttons_enabled = false
+        d.set_url( url )
+        d.show      
+      else
+        d = UI::HtmlDialog.new( { :dialog_title => title, :width => 1000, :height => 600,
+          :style => UI::HtmlDialog::STYLE_DIALOG, :preferences_key => title.gsub(/\s+/, "_") } )
+        d.set_url( url )
+        d.show
+        d.center
+      end  
+    
+    end    
 
 
     def self.show_news
-    # Browse using webdialog
-
-      dlg = UI::WebDialog.new("SketchUp News", true,
-        "SketchUp News", 750, 750, 150, 150, true);
-      dlg.set_url("http://sketchupfordesign.com/news-display/")
-      dlg.navigation_buttons_enabled = false
-      dlg.show
+    # Browse news using webdialog
+    
+      show_url( @exttitle , "http://sketchupfordesign.com/news-display/" ) 
 
     end # show_news
 
@@ -34,9 +49,8 @@ module AS_Extensions
 
     if !file_loaded?(__FILE__)
 
-      # Get the SketchUp help menu
-      help_menu = UI.menu("Help")
-      help_menu.add_item("SketchUp News") { self.show_news }
+      # Add to the SketchUp help menu
+      UI.menu("Help").add_item("SketchUp News") { self.show_news }
 
       # Let Ruby know we have loaded this file
       file_loaded(__FILE__)
